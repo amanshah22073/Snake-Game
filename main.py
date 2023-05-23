f3c77305a3d7f0625a4bfae6e0104868027341a7
Aman Shah <shahaman22073@gmail.com>
2023-05-23 08:38:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from turtle import Screen
+import time
+from snake import Snake
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width = 600 , height = 600)
+screen.title("Snake Game")
+screen.tracer(0)
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+game = True
+while game:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+
+
+
+
+
+
+
